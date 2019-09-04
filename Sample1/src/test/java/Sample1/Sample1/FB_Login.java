fcd2204d47a8c275f38c33f4cc61b50d3ce81e76
admin <admin@10.0.0.30>
2019-09-04 10:19:27 +0530

    Version3

--- Sample1/src/test/java/Sample1/Sample1/FB_Login.java
+++ Sample1/src/test/java/Sample1/Sample1/FB_Login.java
@@ -1,13 +1,21 @@
 package Sample1.Sample1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
+@Test
 public class FB_Login
 {
 	
@@ -15,10 +23,15 @@ public class FB_Login
    
 		String Chromepath= System.getProperty("user.dir");
 		System.out.println(Chromepath);
-		
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("profile.default_content_setting_values.notifications", 2);
+
+		ChromeOptions options = new ChromeOptions();	
+		options.setExperimentalOption("prefs", prefs);
+		options.addArguments("disable-infobars");
 		System.setProperty("webdriver.chrome.driver", Chromepath+"/Drivers//Chromedriver//chromedriver.exe");
 		
-		WebDriver driver= new ChromeDriver();
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com");
 		
@@ -31,13 +44,20 @@ public class FB_Login
 		WebElement Login= driver.findElement(By.xpath("//label[@id='loginbutton']"));
 		Login.submit();
 		
-		//WebElement expand= driver.findElement(By.xpath("//div[@id='userNavigationLabel']"));
-		//expand.click();
+		WebElement expand= driver.findElement(By.xpath("//div[@id='userNavigationLabel']"));
+		expand.click();
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Manage Groups')]")));
 		
-		//WebElement logout= driver.findElement(By.xpath("//span[contains(text(),'Log Out')]"));
-		//logout.click();
+		WebElement groups=driver.findElement(By.xpath("//span[contains(text(),'Manage Groups')]"));
+		groups.click();
+		
+		expand.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'Manage Groups')]")));
+		WebElement logout= driver.findElement(By.xpath("//span[contains(text(),'Log Out')]"));
+		logout.click();
 		
 	
-	driver.quit();;
+	//driver.quit();;
 	}
 }
